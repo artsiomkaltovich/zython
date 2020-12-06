2d0070aa424148832622b6c179d46503602be066
ArtyomKaltovich <KaltovichArtyom@gmail.com>
2020-12-06 21:06:38 +0300

    add var test and minizinc version call

---
+++ test/var/test_range.py
@@ -0,0 +1,18 @@
+import pytest
+
+from zython import var
+
+
+def test_simple():
+    v = var(range(10, 100))
+    assert v.type == range(10, 100)
+
+
+def test_one_arg():
+    v = var(range(100))
+    assert v.type == range(100)
+
+
+def test_step_arg():
+    with pytest.raises(ValueError, match="Step values other than 1 are not supported"):
+        v = var(range(10, 100, 2))
